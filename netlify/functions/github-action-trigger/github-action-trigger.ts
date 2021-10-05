ced19b31e51b6cf68d97d8455c55c4594bdbdfe1
Jesus Dominguez <j.dominguez@kodeline.com>
2021-10-05 20:12:49 +0200

    Use of environment vars

--- netlify/functions/github-action-trigger/github-action-trigger.ts
+++ netlify/functions/github-action-trigger/github-action-trigger.ts
@@ -2,13 +2,13 @@ import { Handler } from '@netlify/functions';
 import { Octokit } from '@octokit/core';
 
 export const handler: Handler = async (event, context) => {
-  const { owner, repository, token } = event.queryStringParameters;
+  const { GH_OWNER: owner, GH_REPOSITORY: repo, GH_TOKEN: token } = process.env;
   const octokit = new Octokit({ auth: token });
 
   try {
     await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
       owner,
-      repo: repository,
+      repo,
       event_type: 'static_build'
     });
 
