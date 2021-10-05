5ef667aebc8af68340b2eca1a193b7e3436c5be7
Jesus Dominguez <j.dominguez@kodeline.com>
2021-10-05 20:04:48 +0200

    Test

--- netlify/functions/github-action-trigger/github-action-trigger.ts
+++ netlify/functions/github-action-trigger/github-action-trigger.ts
@@ -6,7 +6,7 @@ export const handler: Handler = async (event, context) => {
   const octokit = new Octokit({ auth: token });
 
   try {
-    const response = await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+    await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
       owner,
       repo: repository,
       event_type: 'static_build'
@@ -14,7 +14,7 @@ export const handler: Handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: response
+      body: 'ok'
     };
   } catch (err) {
     return {
